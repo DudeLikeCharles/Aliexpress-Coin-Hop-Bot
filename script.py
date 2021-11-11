4034d12ecb556ef9ffc7c0f70bed7d762123c3cf
Robin Šćulac <robin.sculac@gmail.com>
2021-11-11 12:27:29 +0100

    trying out python scripts on

--- script.py
+++ script.py
@@ -3,46 +3,32 @@ from PIL import Image
 import numpy 
 import time
 adb = Client(host='127.0.0.1',port=5037)
-print(adb.devices())
+devices = adb.devices()
 if len (adb.devices()) == 0:
     print("No devices found")
     exit(1)
 device=devices[0]
 
-#create an array from an screenshot
-def get_array(image):
-    return numpy.array(image.getdata(),
-                      dtype=numpy.uint8).reshape(image.size[1],
-                                                 image.size[0],
-                                                 3)
-#position of white pixels in an array
-def get_positions(array):
-    positions = []
-    for i in range(array.shape[0]):
-        for j in range(array.shape[1]):
-            if (array[i][j][0] == 255 and array[i][j][1] == 255 and array[i][j][2] == 255):
-                positions.append((i,j))
-    return positions
-#longest distance between two white pixels
-def get_longest_distance(positions):
-    longest_distance = 0
-    for i in range(len(positions)):
-        for j in range(i+1,len(positions)):
-            distance = abs(positions[i][0]-positions[j][0])+abs(positions[i][1]-positions[j][1])
-            if distance > longest_distance:
-                longest_distance = distance
-    return longest_distance
-
+#create an array from a screenshot
+def screenshot():
+    im = Image.open(device.screencap())
+    im_array = numpy.array(im)
+    return im_array
+#find all the white pixels and return their positions
+def find_white(im_array):
+    white_pixels = []
+    for y in range(0,im_array.shape[0]):
+        for x in range(0,im_array.shape[1]):
+            if im_array[y][x][0] == 255 and im_array[y][x][1] == 255 and im_array[y][x][2] == 255:
+                white_pixels.append((x,y))
+    return white_pixels
 while True:
-    image = device.screencap()
-    with open("screen.png", "wb") as f:
-        f.write(image)
-    image = Image.open("Screen.png")
-    positions = get_positions(get_array(image))
-    pya =get_longest_distance(positions)
-    print(pya)
-
-
+    im_array = find_white(screenshot())
+    if len(im_array) > 0:
+        print("Found white pixels")
+        for pixel in im_array:
+            device.touch(pixel[0],pixel[1],"DOWN_AND_UP")
+            time.sleep(0.1)
     device.shell('input touchscreen swipe 500 500 500 500 '+str(gap))
     time.sleep(2.5)
 
