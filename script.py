35e5e06361e5a6b1d2c52face61004dd71d65662
Robin Šćulac <robin.sculac@gmail.com>
2021-11-11 13:32:33 +0100

    crop top

--- script.py
+++ script.py
@@ -8,12 +8,16 @@ if len (adb.devices()) == 0:
     print("No devices found")
     exit(1)
 device=devices[0]
-#create a deque that puts the biggest elements at the front and the smallest at the back
 
+#crop top 200 pixels of the image
+def crop_top(im_array):
+    im_array = im_array[200:,:,:]
+    return im_array
 #create an array from a screenshot
 def screenshot():
     im = Image.open(device.screencap())
     im_array = numpy.array(im)
+    crop_top(im_array)
     return im_array
 #find all the white pixels and return their positions
 def find_white(im_array):
@@ -37,14 +41,9 @@ def find_longest(white_pixels):
             if distance > longest:
                 longest = distance
     return longest
-
 while True:
     im_array = find_white(screenshot())
-    if len(im_array) > 0:
-        print("Found white pixels")
-        for pixel in im_array:
-            device.touch(pixel[0],pixel[1],"DOWN_AND_UP")
-            time.sleep(0.1)
-    device.shell('input touchscreen swipe 500 500 500 500 '+str(gap))
+
+    device.shell('input touchscreen swipe 500 500 500 500 '+find_longest(im_array))
     time.sleep(2.5)
 
