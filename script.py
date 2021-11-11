fcc90a1d19fe7f8bd46d1e0a572c7853a30e05d5
Robin Šćulac <robin.sculac@gmail.com>
2021-11-11 12:40:01 +0100

    cool python script for playing a game

--- script.py
+++ script.py
@@ -8,6 +8,7 @@ if len (adb.devices()) == 0:
     print("No devices found")
     exit(1)
 device=devices[0]
+#create a deque that puts the biggest elements at the front and the smallest at the back
 
 #create an array from a screenshot
 def screenshot():
@@ -22,6 +23,21 @@ def find_white(im_array):
             if im_array[y][x][0] == 255 and im_array[y][x][1] == 255 and im_array[y][x][2] == 255:
                 white_pixels.append((x,y))
     return white_pixels
+#find longest distance in white pixels pair of x and y
+def find_longest(white_pixels):
+    longest = 0
+    for i in range(0,len(white_pixels)):
+        for j in range(i+1,len(white_pixels)):
+            x_distance = abs(white_pixels[i][0] - white_pixels[j][0])
+            y_distance = abs(white_pixels[i][1] - white_pixels[j][1])
+            if x_distance > y_distance:
+                distance = x_distance
+            else:
+                distance = y_distance
+            if distance > longest:
+                longest = distance
+    return longest
+
 while True:
     im_array = find_white(screenshot())
     if len(im_array) > 0:
